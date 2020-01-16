505614691e67e4d5ce18239a0edc539e4c20bf4c
Dennis Patzer <d.patzer@heimrich-hannot.de>
2020-01-16 16:43:47 +0100

    fixed contentmodel

--- src/Contao/ContentModel.php
+++ src/Contao/ContentModel.php
@@ -1,7 +1,9 @@
 <?php
 
 /*
- * Copyright (c) 2019 Heimrich & Hannot GmbH
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
  *
  * @license LGPL-3.0-or-later
  */
@@ -12,79 +14,73 @@ use Terminal42\DcMultilingualBundle\Model\Multilingual;
 
 class ContentModel extends Multilingual
 {
+    /**
+     * Table name
+     * @var string
+     */
     protected static $strTable = 'tl_content';
 
     /**
      * Find all published content elements by their parent ID and parent table
      *
-     * @param integer $intPid         The article ID
-     * @param string  $strParentTable The parent table name
-     * @param array   $arrOptions     An optional options array
+     * @param integer $intPid The article ID
+     * @param string $strParentTable The parent table name
+     * @param array $arrOptions An optional options array
      *
      * @return Model\Collection|ContentModel[]|ContentModel|null A collection of models or null if there are no content elements
      */
-    public static function findPublishedByPidAndTable($intPid, $strParentTable, array $arrOptions=array())
+    public static function findPublishedByPidAndTable($intPid, $strParentTable, array $arrOptions = [])
     {
-        $t = 'translation';
+        $t = static::$strTable;
 
         // Also handle empty ptable fields
-        if ($strParentTable == 'tl_article')
-        {
-            $arrColumns = array("$t.pid=? AND ($t.ptable=? OR $t.ptable='')");
-        }
-        else
-        {
-            $arrColumns = array("$t.pid=? AND $t.ptable=?");
+        if ($strParentTable == 'tl_article') {
+            $arrColumns = ["$t.pid=? AND ($t.ptable=? OR $t.ptable='')"];
+        } else {
+            $arrColumns = ["$t.pid=? AND $t.ptable=?"];
         }
 
-        if (!static::isPreviewMode($arrOptions))
-        {
-            $time = \Date::floorToMinute();
+        if (!static::isPreviewMode($arrOptions)) {
+            $time         = \Date::floorToMinute();
             $arrColumns[] = "($t.start='' OR $t.start<='$time') AND ($t.stop='' OR $t.stop>'" . ($time + 60) . "') AND $t.invisible=''";
         }
 
-        if (!isset($arrOptions['order']))
-        {
+        if (!isset($arrOptions['order'])) {
             $arrOptions['order'] = "$t.sorting";
         }
 
-        return static::findBy($arrColumns, array($intPid, $strParentTable), $arrOptions);
+        return static::findBy($arrColumns, [$intPid, $strParentTable], $arrOptions);
     }
 
     /**
      * Find all published content elements by their parent ID and parent table
      *
-     * @param integer $intPid         The article ID
-     * @param string  $strParentTable The parent table name
-     * @param array   $arrOptions     An optional options array
+     * @param integer $intPid The article ID
+     * @param string $strParentTable The parent table name
+     * @param array $arrOptions An optional options array
      *
      * @return integer The number of matching rows
      */
-    public static function countPublishedByPidAndTable($intPid, $strParentTable, array $arrOptions=array())
+    public static function countPublishedByPidAndTable($intPid, $strParentTable, array $arrOptions = [])
     {
-        $t = 'translation';
+        $t = static::$strTable;
 
         // Also handle empty ptable fields (backwards compatibility)
-        if ($strParentTable == 'tl_article')
-        {
-            $arrColumns = array("$t.pid=? AND ($t.ptable=? OR $t.ptable='')");
-        }
-        else
-        {
-            $arrColumns = array("$t.pid=? AND $t.ptable=?");
+        if ($strParentTable == 'tl_article') {
+            $arrColumns = ["$t.pid=? AND ($t.ptable=? OR $t.ptable='')"];
+        } else {
+            $arrColumns = ["$t.pid=? AND $t.ptable=?"];
         }
 
-        if (!static::isPreviewMode($arrOptions))
-        {
-            $time = \Date::floorToMinute();
+        if (!static::isPreviewMode($arrOptions)) {
+            $time         = \Date::floorToMinute();
             $arrColumns[] = "($t.start='' OR $t.start<='$time') AND ($t.stop='' OR $t.stop>'" . ($time + 60) . "') AND $t.invisible=''";
         }
 
-        if (!isset($arrOptions['order']))
-        {
+        if (!isset($arrOptions['order'])) {
             $arrOptions['order'] = "$t.sorting";
         }
 
-        return static::countBy($arrColumns, array($intPid, $strParentTable), $arrOptions);
+        return static::countBy($arrColumns, [$intPid, $strParentTable], $arrOptions);
     }
 }
